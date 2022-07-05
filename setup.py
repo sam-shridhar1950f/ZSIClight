c397ef16ae14918de5df2d3803f3076006dc0ebd
Sam Shridhar <sam.shridhar1950f@gmail.com>
2022-07-05 14:20:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 import os
 
 os.system("pip install git+https://github.com/openai/CLIP.git") 
-os.system("pip install sentence-transformers")
+
 
 
 setuptools.setup(
-    name="ZSIClite",
+    name="ZSIClitev2",
     version="1.0",
     author="sam-shridhar1950f",
     author_email="",
     description="Zeroshot Image Classification Lite",
-    long_description="Zero Shot Image Classification (Lighweight Version)",
-    url="https://github.com/sam-shridhar1950f/ZSIClite.git",
+    long_description="Zero Shot Image Classification (Lighweight Version) Version 2",
+    url="https://github.com/sam-shridhar1950f/ZSIClitev2.git",
     packages=setuptools.find_packages(),
     install_requires=[],
     classifiers=[
